9fd48b4f242e1cef0b89230559c0f3b00d5902cf
Sheikh Abdul Wahid <wahidabdul583@gmail.com>
2024-12-07 10:30:43 +0530

    Initial commit - Drinking Water Reminder project

---
+++ main.py
@@ -0,0 +1,12 @@
+import time                     # Importing time module to use delay functionality
+from plyer import notification  # Importing plyer module to display desktop notifications
+
+if __name__ == "__main__":      # Ensures the script runs only when executed directly
+    while True:                 # Infinite loop to send notifications repeatedly
+        notification.notify(
+            title="Please Drink Water Now !!!",  # Title of the notification
+            message="About 3.7 liters of fluids a day for men. About 2.7 liters of fluids a day for women.", # Message to display in the notification
+            app_icon="F:\\Python Learning Journey\\Drink Water Notify Project\\icon.ico",   # Path to the icon file for the notification
+            timeout = 7      # Duration (in seconds) the notification will stay visible
+        )
+        time.sleep(60 * 30)  # Wait for 30 minutes before showing the next notification
